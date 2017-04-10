e49707fd1b1ed3e05d930f30ee24961476c21ee0
jens1o <jenshausdorf@gmail.com>
2017-04-10 14:33:47 +0200

    format code

--- src/extension.ts
+++ src/extension.ts
@@ -65,7 +65,7 @@ export async function activate(context: vscode.ExtensionContext): Promise<void> 
         function spawnServer(...args: string[]): ChildProcess {
             // The server is implemented in PHP
             args.unshift(context.asAbsolutePath(path.join('vendor', 'felixfbecker', 'language-server', 'bin', 'php-language-server.php')));
-            args.push(' --memory-limit=' + memoryLimit)
+            args.push('--memory-limit=' + memoryLimit)
             const childProcess = spawn(executablePath, args);
             childProcess.stderr.on('data', (chunk: Buffer) => {
                 console.error(chunk + '');
